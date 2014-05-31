0e826fa8d780b8d2be03967e1a70a32512d760f1
Nick Graham <nickdud341@gmail.com>
2014-05-31 19:33:30 -0400

    updated phone.py

--- phone.py
+++ phone.py
@@ -1,23 +1,39 @@
-import subprocess
+#!/usr/bin/env python
 import re
 import urllib
 from urllib.request import urlopen
 import requests
+import sys
 
 def landline(phonenumber):
     info = urlopen('http://www.yellowpages.com/reversephonelookup?phone=' + phonenumber)
     html = info.read()
     names = re.compile('(?<=\"fullname\">)([\w\s]+)(?=</a>)')
     lis = names.findall(str(html))
-    print(lis)
+    return(lis)
 
 def mobile(phonenumber):
-    print('Not Done Yet. ' + phonenumber + ' cannot be found')
     url = "https://m.facebook.com/login/identify?ctx=recover"
     phone = { "email": phonenumber }
     req = requests.post(url, data=phone)
     data = req.text.encode("utf8")
     person = re.compile('(?<=<div class="mfsl fcb"><strong>)([\w\s]+)(?=</strong>)')
     name = person.findall(str(data))
-    print (name)
+    return (name)
 
+if (len(sys.argv) > 1):
+    phone = re.compile('\d{10,11}')
+    if (phone.match(sys.argv[1])):
+        landresults = landline(sys.argv[1])
+        cellresults = mobile(sys.argv[1])
+        if (landresults != []):
+            print ('Landline info found:')
+            print (landresults)
+        if(cellresults != []):
+            print('Mobile info found:')
+            print(cellresults)
+        if(landresults == [] and cellresults == []):
+            print('No results found.')
+    else:
+        print('invalid phone input')
+        
