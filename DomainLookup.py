06cd30d63542a5d67e44bd4e0c0065bb8994f92d
Nick Graham <nickdud341@gmail.com>
2014-06-05 15:05:47 -0400

    Rewrote DomainLookup.
    So much cleaner now.

--- DomainLookup.py
+++ DomainLookup.py
@@ -2,21 +2,16 @@
 import os
 import subprocess
 import re
+import socket
 
 def Domain_to_IP(d): #find the IP to a given domain name
-    c = re.compile("\d+\.\d+\.\d+\.\d+") #regular expression to find IP
-    cmd = ['host ' + d] #using the host command
-    #get output of host command
-    result = subprocess.Popen( cmd, stdout=subprocess.PIPE, shell = True ).communicate()[0]
-    ans = c.findall(str(result)) #find the ip address
+    ans = socket.gethostbyname(d) #find the ip address
     try: #return ip
-        return ans[0]
+        return ans
     except: #if finding ip failed, then return a null address
         return '0.0.0.0'
 def IP_to_Domain(ip): #convert ip to domain
-    i = re.compile('\S+\.\S+')
-    cmd = ['host ' + ip] #again with host
-    result = subprocess.Popen( cmd, stdout=subprocess.PIPE, shell = True ).communicate()[0]
-    ans = i.findall(str(result)) #find domain
-    final_ans = ans[1].replace('.\\n\'', '')
-    return final_ans #return result
+    ans = socket.gethostbyaddr(ip) #find domain
+    return ans[0] #return result
+
+       
