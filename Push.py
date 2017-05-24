e9ea1b956d3840d3cb0fb958aab00d1425e03cb2
Nick Graham <nicktgraham@gmail.com>
2017-05-24 17:04:10 -0400

    added geneeric push

---
+++ Push.py
@@ -0,0 +1,22 @@
+from pushbullet import Pushbullet
+import re
+from Dotfile import dotfile
+import requests
+import sys
+import datetime
+import os
+
+PBKEY = ""
+
+def getDefaults():
+    global PBKEY
+    info = dotfile()
+    PBKEY = info.getVal("pushbullet")
+
+getDefaults()
+pb = Pushbullet(PBKEY)
+
+if (len(sys.argv) > 1):
+    pb.push_note("Command", " ".join(sys.argv))
+else:
+    pb.push_note("Command", "\n".join(sys.stdin.readlines()))
